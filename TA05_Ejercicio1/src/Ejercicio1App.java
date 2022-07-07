add6b685993978c1e3a8fe306e53b0c562334c02
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-07 09:56:02 +0200

    Commint Ejercicio1

---
+++ TA05_Ejercicio1/src/Ejercicio1App.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		
+		double variable1 = 3;
+		double variable2 = 12.7;
+		
+		if(variable1 > variable2){
+			System.out.println("La variable mayor es la variable 1 ="+variable1);
+		}
+		else if( variable2 > variable1){
+			System.out.println("La variable mayor es la variable 2 ="+variable2);
+		}
+		else{
+			System.out.println("Ambas variables son iguales");
+		}
+	}
+
+}
