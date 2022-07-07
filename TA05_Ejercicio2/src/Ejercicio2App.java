8435da21d827c71173a8fbde49233b11f0b171f1
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-07 09:59:28 +0200

    Commint Ejercicio2

---
+++ TA05_Ejercicio2/src/Ejercicio2App.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio2App {
+
+	public static void main(String[] args) {
+		
+		
+		String nombre = "Juan Miguel";
+		
+		System.out.println("Hola, Bienvenido");
+		nombre = JOptionPane.showInputDialog("Introduce tu nombre");
+		JOptionPane.showMessageDialog(null, "Hola, Bienvenido"+nombre);
+	}
+
+}
