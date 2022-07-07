9786b0c1d27193d372af32a86b27864303feb05f
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-07 11:35:55 +0200

    Commit Ejercicio7

---
+++ TA05_Ejercicio7/src/Ejercicio7App.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio7App {
+
+	public static void main(String[] args) {
+		
+		int numero =1;
+		while( numero <= 100){
+			JOptionPane.showMessageDialog(null, "Numero"+numero);
+			numero ++;
+		}
+
+	}
+
+}
